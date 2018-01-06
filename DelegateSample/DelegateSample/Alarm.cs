fae68292a605d38cf053970f786fdb6680252a0a
xuyuchends <xuyuchends@163.com>
2018-01-06 14:06:53 +0800

    代理模式

---
+++ DelegateSample/DelegateSample/Alarm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSample
+{
+public    class Alarm
+    {
+        //public void MakeAlert(int param)
+        //{
+        //    Console.WriteLine("alarm 水已经{0}度了", param);
+        //}
+        public void MakeAlert(Object sender,Heater.BoiledEventArgs e)
+        {
+            Heater heater = (Heater)sender;
+            Console.WriteLine("alarm {0} - {1}", heater.type,heater.area);
+            Console.WriteLine("alarm 水已经{0}度了", e.temperature);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DelegateSample/DelegateSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heater heater = new Heater();
+            Alarm alarm = new Alarm();
+            Display display = new Display();
+            heater.boiled += alarm.MakeAlert;
+            heater.boiled += display.ShowMsg;
+            heater.BoilWater();
+            Console.ReadKey();
+        }
+    }
+}
