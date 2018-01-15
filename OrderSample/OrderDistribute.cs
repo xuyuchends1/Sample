27f3fd08f78c61d06853ca5513d34151356d5db0
xuyuchends <xuyuchends@163.com>
2018-01-15 22:22:28 +0800

    正常-> 结构型,桥接模式,扩展方法

---
+++ OrderSample/OrderDistribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSample
+{
+    public class OrderDistribute : IDistribute<Order>
+    {
+        public void DistributeOrder(Order t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class DistributeFactory
+    {
+        public static OrderDistribute Create()
+        {
+            return new OrderDistribute();
+        }
+    }
+    public class DistributeHelper
+    {
+        IDistribute<Order> distribute = DistributeFactory.Create();
+        public void OrderDistribute(Order order)
+        {
+            distribute.DistributeOrder(order);
+        }
+    }
+}
